4c6dfd74b4994da3a90c61f0225ff5ce3fe31d53
danielqparra <queraltoies@gmail.com>
2019-01-19 15:12:10 +0100

    V3

--- NavegadorSwiftWebKitView/SegundoViewController.swift
+++ NavegadorSwiftWebKitView/SegundoViewController.swift
@@ -6,12 +6,20 @@
 //  Copyright © 2019 Daniel Queraltó Parra. All rights reserved.
 //
 
+//---------------------------------------------------------------------------------------------------------------
+//LOS COMENTARIOS SE ENCUENTRAN ENCIMA Y EN CASOS MUY CONCRETOS JUNTO A LO QUE HACEN REFERENCIA
+//---------------------------------------------------------------------------------------------------------------
+
 import UIKit
+//IMPORTAMOS LAS LIBRERIAS DE LA BASE DE DATOS QUE USAREMOS
 import SQLite3
 
-class SegundoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SegundoViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
     var db: OpaquePointer?
     var historial = [Histo]()
+    var histo: [String] = []
+
+    
 
     @IBOutlet weak var histoTableView: UITableView!
     
@@ -22,30 +30,62 @@ class SegundoViewController: UIViewController, UITableViewDelegate, UITableViewD
 
         // Do any additional setup after loading the view.
     }
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        return (historial.count)
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
+    //---------------------------------------------------------------------------------------------------------------
+    //VISUALIZAR HISTORIAL EN TABLEVIEW
+    //---------------------------------------------------------------------------------------------------------------
+    //INDICAMOS EL NUMERO DE FILAS QUE TENDRA NUESTRA SECCIÓN A PARTIR DEL TOTAL DE OBJETOS QUE SE HABRAN CREADO GRACIAS A NUESTRA BASE DE DATOS
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return historial.count
+    }
+    //IPOR CADA REGISTRO CREAMOS UNA LINEA Y LA RELLENAMOS CON LOS OBJETOS EXTRAIDOS DE LA BASE DE DATOS
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let celda = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "celdilla")
+        //INDICAMOS EL ESTILO DE LA CELDA Y EL IDENTIFICADOR DE ESTA
+        let celda = UITableViewCell(style: UITableViewCell.CellStyle.default,  reuseIdentifier: "celdilla")
+        //RECCOREMOS NUESTRA COLECCIÓN DE OBJETOS Y GUARDAMOS LA URL DE NUESTRO HISTORIAL EN UNA COLECCION DE STRINGS PARA PODER RELLENAR LAS CELDAS A CONTINUACION
         for hi in historial{
-            celda.textLabel?.text = hi.url
+            histo.append(hi.url!)//AÑADIMOS EL ESTRING "URL" A LA NUEVA COLECCION
         }
-        return (celda)
+        //RELLENAMOS LAS CELDAS CON NUESTRA NUEVA COLECCION
+        celda.textLabel?.text = histo[indexPath.row]//LE INDICAMOS QUE LOS INSERTE SEGUN EL INDICE DE FILAS QUE CREAMOS EN LA FUNCION ANTERIOR CON "historial.count"
+        //CARGAMOS LAS CELDAS
+        return celda
     }
+    
+    
+
+
+    
+
+    //---------------------------------------------------------------------------------------------------------------
+    //BOTONES
+    //---------------------------------------------------------------------------------------------------------------
 
     
     @IBAction func salir(_ sender: Any)
     {
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func eliminar(_ sender: Any) {
+        //ELIMINAMOOS TODOS LOS REGISTROS DE LA BASE DE DATOS
         eliminarHistorial()
+        //VOLVEMOS A MIRAR SI HAY DATOS EN LA BASE DE DATOS
+        leerValores()
+        //RECARGAMOS EL TABLEVIEW
+        histoTableView.reloadData()
     }
     
+    //---------------------------------------------------------------------------------------------------------------
+    //BASE DE DATOS
+    //---------------------------------------------------------------------------------------------------------------
+
     func conectarDB()
     {
         let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
@@ -67,30 +107,30 @@ class SegundoViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func leerValores(){
         
-        //first empty the list of histo
+        //PRIMERO LIMPIAMOS LA LISTA "HISTORIAL"
         historial.removeAll()
         
-        //this is our select query
+        //GUARDAMOS NUESTRA CONSULTA
         let queryString = "SELECT * FROM Historial"
         
-        //statement pointer
+        //PUNTERO DE INSTRUCCIÓN
         var stmt:OpaquePointer?
         
-        //preparing the query
+        //PREPARACIÓN DE LA CONSULTA
         if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             print("error preparing insert: \(errmsg)")
             return
         }
         
-        //traversing through all the records
+        //RECORREMOS LOS REGISTROS
         while(sqlite3_step(stmt) == SQLITE_ROW){
             let id = sqlite3_column_int(stmt, 0)
             let url = String(cString: sqlite3_column_text(stmt, 1))
             
             
-            //adding values to list
-            historial.append(Histo(id: Int(id), url: String(describing: url)))
+        //AÑADIMOS LOS VALORES A LA LISTA
+        historial.append(Histo(id: Int(id), url: String(describing: url)))
 
         }
         
@@ -98,16 +138,19 @@ class SegundoViewController: UIViewController, UITableViewDelegate, UITableViewD
  
     func eliminarHistorial()
     {
-
+        //GUARDAMOS NUESTRA CONSULTA
         let queryString = "DELETE FROM Historial"
+        //CREAMOS EL PUNTERO DE INSTRUCCIÓN
         var deleteStatement: OpaquePointer? = nil
         
+        //PREPARACIÓN DE LA CONSULTA
         if sqlite3_prepare(db, queryString, -1, &deleteStatement, nil) != SQLITE_OK{
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             print(queryString)
             print("error preparing insert: \(errmsg)")
             return
         }
+        //ELIMINAMOS LOS REGISTROS
         if sqlite3_prepare_v2(db, queryString, -1, &deleteStatement, nil) == SQLITE_OK {
             if sqlite3_step(deleteStatement) == SQLITE_DONE {
                 print("Successfully deleted row.")
@@ -118,11 +161,8 @@ class SegundoViewController: UIViewController, UITableViewDelegate, UITableViewD
             print("DELETE statement could not be prepared")
         }
         
+        //FINALIZAMOS LA SENTENCIA
         sqlite3_finalize(deleteStatement)
-        
-        histoTableView.reloadData()
-        print(leerValores())
-        
 
     }
     
--- NavegadorSwiftWebKitView/ViewController.swift
+++ NavegadorSwiftWebKitView/ViewController.swift
@@ -6,15 +6,20 @@
 //  Copyright © 2019 Daniel Queraltó Parra. All rights reserved.
 //
 
+//---------------------------------------------------------------------------------------------------------------
+//LOS COMENTARIOS SE ENCUENTRAN ENCIMA Y EN CASOS MUY CONCRETOS JUNTO A LO QUE HACEN REFERENCIA
+//---------------------------------------------------------------------------------------------------------------
 import UIKit
+//IMPORTAMOS LAS LIBRERIAS DE NAVEGACIÓN WEBKIT VIEW Y DE LA BASE DE DATOS QUE USAREMOS
 import WebKit
 import SQLite3
 
 
 class ViewController: UIViewController, WKUIDelegate, UISearchBarDelegate, WKNavigationDelegate {
+    //VARIABLES PARA LA BASE DE DATOS Y LOS OBJEROS
     var db: OpaquePointer?
     var historial = [Histo]()
-    
+    //COSAS QUE USAREMOS
     @IBOutlet weak var barraDeBusqueda: UISearchBar!
     @IBOutlet weak var webKitView: WKWebView!
     @IBOutlet weak var retroceder: UIBarButtonItem!
@@ -29,13 +34,14 @@ class ViewController: UIViewController, WKUIDelegate, UISearchBarDelegate, WKNav
         // Do any additional setup after loading the view, typically from a nib.
         barraDeBusqueda.delegate = self
         webKitView.navigationDelegate = self
-        webKitView.load(URLRequest(url: URL(string: "http://www.google.com")!))
-        crearBD()
+        webKitView.load(URLRequest(url: URL(string: "https://www.google.com")!))//
+        crearBD()//CREAMOS O ABRIMOS(SI YA EXISTE) LA BASE DE DATOS
     }
 
     //---------------------------------------------------------------------------------------------------------------
-    //BUSCADOR
+    //BOTONES
     //---------------------------------------------------------------------------------------------------------------
+    //RETROCEDEMOS A LA PAGINA ANTERIOR
     @IBAction func atras(_ sender: Any)
     {
         if webKitView.canGoBack
@@ -43,12 +49,12 @@ class ViewController: UIViewController, WKUIDelegate, UISearchBarDelegate, WKNav
             webKitView.goBack()
         }
     }
-    
+    //RECARGAMOS LA PAGINA
     @IBAction func recargar(_ sender: Any)
     {
         webKitView.reload()
     }
-    
+    //AVANZAMOS A LA PAGINA ANTERIOR
     @IBAction func siguiente(_ sender: Any)
     {
         if webKitView.canGoForward
@@ -61,104 +67,84 @@ class ViewController: UIViewController, WKUIDelegate, UISearchBarDelegate, WKNav
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
     {
+        //ALMACENAMOS EL CONTENIDO DE LA BARRA DE BUSQUEDA
         let barraBusqueda = barraDeBusqueda.text!
-        
+        //CREAMOS CONSTANTES CON DIVERSA COSAS QUE USAREMOS PARA TRATAR LA/LAS URL/URLS
         let com: String = ".com"
         let es: String = ".es"
         let net: String = ".net"
-        let http: String = "https://"
+        //let http: String = "http://"
+        let https: String = "https://"
         let www: String = "www."
         let google: String = "https://www.google.com/search?q="
-        
+        //CONVERTIMOS LOS "ESPACIOS"EN LA BARRA DE BUSQUEDA POR "-" Y LA ALMACENAMOS PARA TRABAJAR CON ELLA
         var barraBusquedafinal = barraBusqueda.replacingOccurrences(of: " ", with: "-", options: .literal, range: nil)
-
+        //QUITAMOS LA PRIORIDAD DE LA BARRA
         barraDeBusqueda.resignFirstResponder()
+        //PASAMOS A MINUSCULAS EL CONTENIDO DE LA VARIABLE DEFINITIVA(barraBusquedafinal)
         barraBusquedafinal = barraBusquedafinal.lowercased()
-        
-        print("0")
-
+        //COMPROBAMOS SI EL CONTENIDO DE LA VARIABLE DEFINITIVA TIENE ".COM", ".ES", ".NET", ""
         if !barraBusquedafinal.contains(com) && !barraBusquedafinal.contains(es) && !barraBusquedafinal.contains(net)
         {
+            //SI NO LOS TIENE LE AÑADIMOS LA URL DE BUSQUEDA POR DEFECTO DE GOOGLE
             barraBusquedafinal = google + barraBusquedafinal
-            print(barraBusquedafinal)
-            print("4")
-            
+            //Y LE DECIMOS QUE PASE NUESTRO STRING A FOMATO URL(ALMACENANDOLO EN UNA CONSTANTE) Y LO USAMOS PARA QUE SI NO SE PUDIERA PASAR NO HICIERA NADA
             if let url = URL(string: barraBusquedafinal)
             {
-                print("siiiiiiiiii")
-                
-                let myRequest = URLRequest(url: url)
-                webKitView.load(myRequest)
-                
-                
+                let myRequest = URLRequest(url: url)//HACEMOS LA SOLICITUD DE CARGA
+                webKitView.load(myRequest)//AQUI ES CUANDO LA CARGA
             }
             
         }
         else
         {
-            print("1")
+            //COMPROBAMOS SI NUESTRA DIRECCION (CONTENIDA EN LA BARRA DE BUSQUEDA) POSEE "WWW."
             if !barraBusquedafinal.contains(www)
             {
-                barraBusquedafinal = www + barraBusquedafinal
-                print(barraBusquedafinal)
-                print("2")
+                barraBusquedafinal = www + barraBusquedafinal//SI NO LO POSEE SE LO AÑADIMOS
             }
-            if !barraBusquedafinal.contains(http)
+            /*if !barraBusquedafinal.contains(http)
             {
                 barraBusquedafinal = http + barraBusquedafinal
-                print(barraBusquedafinal)
-                print("3")
-                
             }
-            
+            */
+            //COMPROBAMOS SI NUESTRA DIRECCION (CONTENIDA EN LA BARRA DE BUSQUEDA) POSEE  "HTTPS://"
+            if !barraBusquedafinal.contains(https)
+            {
+                barraBusquedafinal = https + barraBusquedafinal//SI NO LO TIENE SE LO AÑADIMOS
+            }
+            //Y LE DECIMOS QUE PASE NUESTRO STRING A FOMATO URL(ALMACENANDOLO EN UNA CONSTANTE) Y LO USAMOS PARA QUE SI NO SE PUDIERA PASAR NO HICIERA NADA
             if let url = URL(string: barraBusquedafinal)
             {
-                print("siiiiiiiiii")
-                
-                let myRequest = URLRequest(url: url)
-                webKitView.load(myRequest)
+                let myRequest = URLRequest(url: url)//HACEMOS LA SOLICITUD DE CARGA
+                webKitView.load(myRequest)//AQUI ES CUANDO LA CARGA
             }
 
             
             
         }
-
-    }
-
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!)
-    {
-        UIApplication.shared.isNetworkActivityIndicatorVisible = false
-        if webKitView.canGoForward
-        {
-            avanzar.isEnabled = true
-        }
-        else
-        {
-            avanzar.isEnabled = false
-        }
-        if webKitView.canGoBack
-        {
-            retroceder.isEnabled = true
-        }
-        else
-        {
-            retroceder.isEnabled = false
-        }
     }
     
+    //CAPTURAMOS LA ACCION QUE SE HARAN DESPUES DE QUE EL WEBKIT VIEW TERMINE SU TRABAJO
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!)
     {
         barraDeBusqueda.text = webKitView.url?.absoluteString
         insertar()
     }
     
+    //CAPTURAMOS LA ACCION QUE SE HARAN ANTES QUE  EL WEBKIT VIEW EMPIECE SU TRABAJO
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        //REVISAMOS QUE SE PUEDE HACER CON LOS BOTONES
+        activarBotones()
+    }
     
     //---------------------------------------------------------------------------------------------------------------
     //FUNCIONALIDADES
     //---------------------------------------------------------------------------------------------------------------
     
-    func activarBotones()
+    func activarBotones()//HABILITAMOS O DESHABILITAMOS LOS BOTONES DE AVANCE O RETROCESO SEGÚN SE PUEDA
     {
         if webKitView.canGoForward
         {
@@ -177,9 +163,6 @@ class ViewController: UIViewController, WKUIDelegate, UISearchBarDelegate, WKNav
             retroceder.isEnabled = false
         }
     }
-    
-    
-
     
 
     //---------------------------------------------------------------------------------------------------------------
@@ -187,35 +170,30 @@ class ViewController: UIViewController, WKUIDelegate, UISearchBarDelegate, WKNav
     //---------------------------------------------------------------------------------------------------------------
     func crearBD()
     {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //INDICAMOS DONDE SE GUARDARA LA BASE DE DATOS Y EL NOMBRE DE ESTAS
         let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
             .appendingPathComponent("Historial.sqlite")
-        
+        //INDICAMOS SI DIERA ALGUN FALLO AL CONECTARSE
         if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
             print("error opening database")
         }
-        else {
+        else {//SI PODEMOS CONECTARNOS A LA BASE DE DATOS CREAREMOS LA ESTRUCTURA DE ESTA, SI NO EXISTIERA NO SE HARIA NADA
             print("base abierta")
             if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Historial (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT)", nil, nil, nil) != SQLITE_OK {
                 let errmsg = String(cString: sqlite3_errmsg(db)!)
                 print("error creating table: \(errmsg)")
             }
         }
-        //insertar()
-        //leerValores()
-        for hi in historial {
-            print(hi.url!)
-        }
+
     }
         
     func insertar()  {
-        //creating a statement
+        //CREAMOS EL PUNTERO DE INSTRUCCIÓN
         var stmt: OpaquePointer?
         
-        //the insert query
+        //CREAMOS NUESTRA SENTENCIA
         let queryString = "INSERT INTO Historial (url) VALUES ("+"'"+String(barraDeBusqueda.text!)+"')"
-        //preparing the query
+        //PREPARAMOS LA SENTENCIA
         if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             print(queryString)
@@ -224,57 +202,22 @@ class ViewController: UIViewController, WKUIDelegate, UISearchBarDelegate, WKNav
         }
         
         
-        //executing the query to insert values
+        //EJECUTAMOS LA SENTENCIA PARA INSERTAR LOS VALORES
         if sqlite3_step(stmt) != SQLITE_DONE {
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             print("fallo al insertar en historial: \(errmsg)")
             return
         }
         
-        
+        //FINALIZAMOS LA SENTENCIA
+        sqlite3_finalize(stmt)
         //displaying a success message
         print("Histo saved successfully")
-        //print(leerValores())
-    }
-    
-    func leerValores(){
-        
-        //first empty the list of heroes
-        //historial.removeAll()
-        
-        //this is our select query
-        let queryString = "SELECT * FROM Historial"
-        
-        //statement pointer
-        var stmt:OpaquePointer?
-        
-        //preparing the query
-        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
-            let errmsg = String(cString: sqlite3_errmsg(db)!)
-            print("error preparing insert: \(errmsg)")
-            return
-        }
-        
-        //traversing through all the records
-        while(sqlite3_step(stmt) == SQLITE_ROW){
-            let id = sqlite3_column_int(stmt, 0)
-            let url = String(cString: sqlite3_column_text(stmt, 1))
-            
-            
-            //adding values to list
-            historial.append(Histo(id: Int(id), url: String(describing: url)))
-            
-        }
-        
         
     }
-    
-
-    
-        
 }
 //---------------------------------------------------------------------------------------------------------------
-//BASE DE DATOS
+//OBJETOS
 //---------------------------------------------------------------------------------------------------------------
 
 class Histo{
